a7dca653d38af5931838ea4f066c98e1d75bad11
sayan4545 <sayanchatterjee3010@gmail.com>
2025-04-14 23:35:59 +0530

    Predicarte, Consumer,Suppluier,Function

--- Java8Features/LambdaExpression/basics.java
+++ Java8Features/LambdaExpression/basics.java
@@ -1,15 +1,16 @@
 package Java8Features.LambdaExpression;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
-
+import java.util.function.Supplier;
 public class basics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         Thread t1 = new Thread(new Task1());
         t1.start();
-
         Thread t2 = new Thread(()->{
             System.out.println("task2");
         });
@@ -25,7 +26,6 @@ public class basics {
         System.out.println(predicate.test(8));// should return true
         Predicate<String> isStartsWithA = x -> x.charAt(0) == 'A';
         System.out.println(isStartsWithA.test("Anuska"));// Should return true;
-
         System.out.print("Enter a character to compare: ");
         char inputChar = sc.next().charAt(0);
         Predicate<String> isStartsWithDynamically = x -> x.charAt(0) == inputChar;
@@ -43,6 +43,40 @@ public class basics {
         combinedAnd.test("Chandrika Dey");
         //int[] numbers = {1, 2, 3, 4, 5};
         //System.out.println( ls.search(numbers,sc.nextInt()));
+        // Function
+        Function<Integer,Integer> doubleiIt = x -> x*2; 
+        Integer result = doubleiIt.apply(10);
+        System.out.println(result + " //  //  // Function impl");
+        Function<Integer,Integer> tripleIt = x -> 3*x;
+        System.out.println(tripleIt.andThen(doubleiIt).apply(3)); // 18
+        Function<Integer,Integer> fun = Function.identity();
+        int res = fun.apply(5);
+        System.out.println(res + " o/p of res");
+        // Consumer
+        Consumer<Integer> print = (x)-> System.out.println(x);
+        print.accept(89);
+        List<Integer> list = Arrays.asList(1,2,3);
+        Consumer<List<Integer>> conumeList = x ->{
+            for (int i : x) {
+                System.out.print(i + " ");
+            }
+        };
+        conumeList.accept(list);
+        // supplier
+        Supplier<String> givehelloWorld = () -> "hello World";
+        String str = givehelloWorld.get();
+        System.out.println(str);
+
+        Predicate<Integer> predicate2 = x -> x%2==0;
+        Consumer<Integer> consumer = x -> System.out.println(x);
+        Supplier<Integer> supplier = () -> 10;
+        Function<Integer,Integer> function = x -> x*x;
+
+        if(predicate2.test(supplier.get())){
+            consumer.accept(function.apply(supplier.get()));
+        }
+
+        
     }    
 }
 class Task1 implements Runnable {
