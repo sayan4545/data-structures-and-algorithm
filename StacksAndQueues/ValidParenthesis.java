0d7ff4e23a5db99557d5e63c164733276ad5b296
sayan4545 <sayanchatterjee3010@gmail.com>
2025-04-17 23:28:21 +0530

    rectified the previous code

--- StacksAndQueues/ValidParenthesis.java
+++ StacksAndQueues/ValidParenthesis.java
@@ -1,56 +1,39 @@
 import java.util.Stack;
-
 public class ValidParenthesis {
     public static void main(String[] args) {
         Stack<Character> stack1 = new Stack<>();
         String str = "((()))";
         System.out.println(isValid(str));
     }
-    public static boolean isValid(String str){
-        Stack<Character> st = new Stack<>();
-        for(int i=0;i<str.length();i++){
-            if(st.size()==0){
-                st.push(str.charAt(i));
-                continue;
-            }
 
 
-            if(st.peek()=='(' || st.peek()=='{' ||st.peek()=='['){
-                st.push(str.charAt(i));
-            }
-            
-            else{
-                //if(st.isEmpty()) return false;
-                if(str.charAt(i)==')'){
-                    if(st.peek()=='('){
-                        st.pop();
-                    }
-                }
-                else{
-                    return false;
-                }
-                if(str.charAt(i)==']'){
-                    if(st.peek()=='['){
-                        st.pop();
-                    }
-                }
-                else{
-                    return false;
-                }
-                if(str.charAt(i)=='}'){
-                    if(st.peek()=='{'){
-                        st.pop();
-                    }
-                }
-                else{
-                    return false;
-                }
-            }
-            
+public static boolean isValid(String str) {
+Stack<Character> st = new Stack<>();
+for (int i = 0; i < str.length(); i++) {
+    char current = str.charAt(i);
+
+    // Push opening brackets onto the stack
+    if (current == '(' || current == '{' || current == '[') {
+        st.push(current);
+    } else {
+        // If the stack is empty, the closing bracket is invalid
+        if (st.isEmpty()) {
+            return false;
         }
-        return st.isEmpty();
-       
-       
+        char top = st.peek();
+        
+        // Check for matching opening bracket at the top of the stack
+        if ((current == ')' && top == '(') ||
+            (current == ']' && top == '[') ||
+            (current == '}' && top == '{')) {
+            st.pop(); // Remove the matched opening bracket
+        } else {
+            return false; // Mismatched bracket
+        }
     }
-    
 }
+return st.isEmpty();
+}
+}
+    
+
