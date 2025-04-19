7da839568c595bee4c8e429c2451806231402427
sayan4545 <sayanchatterjee3010@gmail.com>
2025-04-20 02:26:40 +0530

    added leetcode description links

--- Tree/MaxDepthOfATree.java
+++ Tree/MaxDepthOfATree.java
@@ -9,12 +9,14 @@ public class MaxDepthOfATree {
             this.left = null;
         }
     }
+    //https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
     public int MaxDepth(Node root){
         if (root == null) return 0;
         int rightHeight = MaxDepth(root.right);
         int leftHeight = MaxDepth(root.left);
         return Math.max(leftHeight, leftHeight) +1;
     }
+    // https://leetcode.com/problems/diameter-of-binary-tree/
     public int diameterOfBinaryTree(Node root){
         if (root == null) return 0;
         int option1 = diameterOfBinaryTree(root.left);
