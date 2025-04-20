4d69c065e449b051ea7db9cb6e7d43f31841c6f8
sayan4545 <sayanchatterjee3010@gmail.com>
2025-04-21 01:03:57 +0530

    Added  code to return if the binary tree is balanced or not

--- Tree/MaxDepthOfATree.java
+++ Tree/MaxDepthOfATree.java
@@ -24,4 +24,22 @@ public class MaxDepthOfATree {
         int option3 = MaxDepth(root.left) + MaxDepth(root.right);
         return Math.max(option1,Math.max(option2, option3));
     }
+    public boolean isBalancedBinaryTree(Node node){
+        // base case
+        if(node == null) return true;
+        // ist case
+        int lh = MaxDepth(node.left);
+        int rh = MaxDepth(node.right);
+        int diff = Math.abs(lh-rh);
+        boolean ans1 = (diff <=1);
+        
+        // problem for recursion
+        boolean ans2 = isBalancedBinaryTree(node.left);
+        boolean ans3 = isBalancedBinaryTree(node.right);
+
+        return (ans1 && ans2 && ans3);
+
+
+    }
+
 }
